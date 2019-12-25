d33aa22e12d38ccc7f544d691637ae9e1c6542cd
noicomdien1102 <havantien1902@gmail.com>
2019-12-25 18:39:22 +0700

    SQL

--- SQL/Assignment2/ASSI 2.sql
+++ SQL/Assignment2/ASSI 2.sql
@@ -1,3 +1,4 @@
+-- Exercise 1:Design a table
 DROP database IF EXISTS QUANLYFRESHER;
 CREATE DATABASE IF NOT EXISTS QUANLYFRESHER;
 USE QUANLYFRESHER;
@@ -16,6 +17,31 @@ CREATE TABLE TRAINEE
    EVALUATION                NVARCHAR(200)
 );
 
+-- Question 2: thêm trường VTI_Account với điều kiện not null & unique
+ALTER TABLE Trainee
+	ADD COLUMN VTI_Account		NVARCHAR(30) NOT NULL UNIQUE;
+    
 
+	-- Exercise 2: Data Types
+DROP TABLE IF EXISTS Bang1;
+CREATE TABLE Bang1
+(
+	ID1					MEDIUMINT AUTO_INCREMENT PRIMARY KEY,
+    Name_ID1			NVARCHAR(35) NOT NULL,
+    Code				TINYINT CHECK((Code >= 0) AND (Code <=5)),
+    ModifiedData_ID1	DATETIME
+);
+
+
+	-- Exercise 3: Data Types (2)
+DROP TABLE IF EXISTS Bang2;
+CREATE TABLE Bang2
+(
+	ID2					MEDIUMINT AUTO_INCREMENT PRIMARY KEY,
+    Name_ID2			NVARCHAR(40) NOT NULL,
+    BrithDate_ID2		DATE NOT NULL,
+    Gender_ID2			ENUM('male','female','unknown'),
+    IsDeletedFlag_ID2	TINYINT
+);
 
 
